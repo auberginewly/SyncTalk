d1a3d87297c14c3f92d1237c12e8e2863d0e1d49
aubergine <aubergine@auberginedeMacBook-Pro.local>
2025-09-26 19:02:29 +0800

    Initial commit: Add backend with Express.js, MongoDB connection, and auth routes

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,14 @@
+export async function signup(req, res) {
+  // 注册逻辑
+  res.send('Signup Route');
+}
+
+export async function login(req, res) {
+  // 登录逻辑
+  res.send('Login Route');
+}
+
+export async function logout(req, res) {
+  // 登出逻辑
+  res.send('Logout Route');
+}
---
+++ backend/src/server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+// import dotenv from 'dotenv';    
+// dotenv.config();
+// 使用 'dotenv/config' 直接加载环境变量 这样也行
+import 'dotenv/config';
+import authRoutes from './routes/auth.route.js';
+import { connectDB } from './lib/db.js';
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  // 启动服务器前连接数据库
+  connectDB();
+});
